5da9c1641d989efe345a7fefcb75ecdd9672ccea
Srinithi1101 <srinithik47@gmail.com>
2026-07-23 10:00:13 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,52 @@
+print("Student Introduction")
+a=input("Enetr Student Name:")
+b=input("Enter Age:")
+c=input("Enter City:")
+print(f"Student name:{a}")
+print(f"Age:{b}")
+print(f"City:{c}")
+
+print("Employee Profile Program")
+a=input("Enter Employee name:")
+b=input("Enter Employee ID:")
+c=input("Enter Department:")
+d=input("Enter salary:")
+print(f"Employee name:{a}")
+print(f"Employee ID:{b}")
+print(f"Department:{c}")
+print(f"Salary:{d}")
+
+
+print("Personal Profile")
+a=input("Enter Name:")
+b=input("Enter Age:")
+c=input("Enter Height:")
+d=input("Eneer Weight:")
+print(f"Name:{a}")
+print(f"Age:{b}")
+print(f"Height:{c}cm")
+print(f"weight:{d}kg")
+
+print("Data Type Checker")
+int_value=10
+float_value=10.5
+string_value="Python"
+boolean_value=True
+print(f"Integer Value:{int_value}")
+print(f"Datatype:{type(int_value)}")
+print(f"float_value:{float_value}")
+print(f"Datatype:{type(float_value)}")
+print(f"string_value:{string_value}")
+print(f"Datatype:{type(string_value)}")
+print(f"boolean_value:{boolean_value}")
+print(f"Datatype:{type(boolean_value)}")
+print()
+
+print()
+num=40.6
+b=int(num)
+print("Original value:",num)
+print("Original data type:",type(num))
+print("Converted value:",b)
+print("Converterd data type:",type(b))
+print()
