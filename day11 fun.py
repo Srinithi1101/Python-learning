c596e5f91a64e578167261c598159353a3639b91
Srinithi1101 <srinithik47@gmail.com>
2026-07-29 19:35:24 +0530

    Add files via upload

---
+++ day11 fun.py
@@ -0,0 +1,80 @@
+#Function
+# def add():
+#     x=10
+#     y=5
+#     z=x+y
+#     print(z)
+# add()   
+
+
+#argument(single tym print)
+# def wish(name):
+#     print(f"Happy birthday {name}!")
+# wish("dave") 
+# wish("Sri")   
+
+#join age and name
+
+# def wish(name,age):
+#     print(f"Happy birthday {name}!")
+#     print(f"You are {age} years old!")
+# wish("SRI",20)
+# wish("John",22)    
+
+
+#sigle tym add,sub 
+# def add(x,y):
+#     print(x+y)
+# def sub(x,y):
+#     print(x-y)
+# def mul(x,y):
+#     print(x*y)    
+# def div(x,y):
+#     print(x/y)
+# add(2,3)
+# sub(4,2)
+# mul(2,4)
+# div(4,2)    
+
+# def hello(name):
+#     print("hello",name)
+
+# n=input("Enter your value :") 
+# hello(n)      
+
+
+#task=1:
+
+# def squ(n):
+#     print(n**2)
+# user=int(input("Enter the value:"))
+# squ(user)
+
+
+#Return
+# def add(x,y):
+#     z=x+y
+#     return 2
+# print(add (5,6))
+
+#bill
+# def bill(price):
+#     discount=100
+#     return price -discount
+# print(bill(2000))
+
+# def list(n):
+#     for i in n:
+#         print(i*2)
+# list([20,30,40,50])
+
+# def mul(list):
+#     for i in list:
+#         print(i*2)
+# n=([23,30,40])
+# mul(n)
+
+
+
+
+       
