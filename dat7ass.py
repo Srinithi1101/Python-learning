f70de958c1c247b348f5e459fed4e25e6a8a75a5
Srinithi1101 <srinithik47@gmail.com>
2026-07-29 19:28:02 +0530

    Add files via upload

---
+++ dat7ass.py
@@ -0,0 +1,62 @@
+ #1 to n
+#n=int(input("Enetr the n:"))
+#for i in range(1,n+1):
+#    print(i)
+#print()
+#N to 1
+#n=int(input("Enter the n:"))
+#   print(i)
+#even number(1 to n)
+#n=int(input("Enter the n:"))
+#for i in range(1,n+1):
+#    if i%2==0:
+ #       print(i)
+#print()
+#odd number
+#n=int(input("Enter the n:"))
+#for i range(1,n+1):
+#   if i%3==0:
+ #       print(i)
+#print()
+#first n numbers
+#n=int(input("Enter the n:"))
+#for i in range(1,n+1):
+#   print(i)
+
+#first n even number
+n=int(input("Enter the n:"))
+for i in range(1,n+1):
+    print(2*i)
+print()
+
+#n odd number
+
+n=int(input("Enter the n:"))
+for i in range(1,n+1):
+    print(2*i+1)
+print()
+#sum of num 1 to n
+n=int(input("Enter the n:"))
+sum=0
+for i in range(1,n+1):
+    sum+=i
+    print(sum)
+print()
+#sum even num 1 to n
+n=int(input("Enter the n:"))
+sum=0
+for i in range(2,n+1,2):
+    sum+=i
+    print(sum)
+print()
+#sum odd num 1 to n
+n=int(input("Enter the n:"))
+sum=0
+for i in range(1,n+1,2):
+    sum+=i
+    print(sum)
+print()
+    
+
+    
+    
