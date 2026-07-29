fc1ce02c9096710775005cdecdf7894cf6d7eb73
Srinithi1101 <srinithik47@gmail.com>
2026-07-29 19:27:33 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,33 @@
+#counter varaible
+# for i in range(5):
+#     print("*",end="")
+
+# for j in range(5):
+#     for i in range(5):
+#       print("*",end=" ") 
+#     print() 
+
+# for j in range(5):
+#    for i in range(5):
+#       print("A",end=" ") 
+#    print() 
+
+# n= int(input("Enter the number:"))
+# for i in range(1,11):
+#    print (n,"X",i,"=",n*i)      
+
+# i=1
+# add=0
+# while i<=5:
+#    add+=i
+#    i+=1
+# print(add)
+
+# num=5
+# i=1
+# fact=2 
+# while i<=num:
+#     fact*=i
+#     i*=1
+#  print(fact)
+
