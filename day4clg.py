b0ca6068109300b296fd21af27796f73370a94b2
Srinithi1101 <srinithik47@gmail.com>
2026-07-29 18:19:16 +0530

    Add files via upload

---
+++ day4clg.py
@@ -0,0 +1,41 @@
+
+#Eligible for vote
+age=int(input("Enter your age:")) 
+if age>=18:
+    print("You can vote") 
+else:
+    print("you can't vote")
+    print()
+    
+#odd or even:
+a=int(input("Enter the number:"))
+if a%2==0:
+    print("The number is even")
+else:
+    print("The number is odd")
+print()    
+
+# Grade of Student using mark
+marks=int(input("Enter the mark:"))
+if marks >=90:
+    print("Grade A") 
+elif marks >=80:
+    print("Grade B") 
+elif marks >=70:
+    print("Grade c")
+elif marks >=60:
+    print("Grade E")
+else:
+    print("Grade F")
+print()    
+    
+#largest two numbers
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+if a>b:
+      print("Largest number is ",a)
+else:
+    print("Largest number is",b)
+print()
+    
+
