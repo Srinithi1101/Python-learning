dbaebdf28932b7096f97b0736c18284b38ad0837
Srinithi1101 <srinithik47@gmail.com>
2026-07-29 20:47:26 +0530

    Add files via upload

---
+++ day13.ass.py
@@ -0,0 +1,15 @@
+#Use the math MOdule
+import math
+print("Square Root:",math.sqrt(64))
+print("pi:",math.pi)
+print()
+
+#Use the random Module
+import random
+num=random.randint(1,20)
+print("Random Number:",num)
+print()
+
+#Explore an Installed package
+import math
+print(dir(math))
