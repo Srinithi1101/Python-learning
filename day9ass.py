eed30c67e4ef8f0d2a3e8c004dd031cc659ea799
Srinithi1101 <srinithik47@gmail.com>
2026-07-29 19:32:44 +0530

    Add files via upload

---
+++ day9ass.py
@@ -0,0 +1,27 @@
+#capitalize
+name="srinithi"
+print(name.capitalize())
+print()
+#only digits
+n=input("Enter the number:")
+if n.isdigit():
+    print("Only digits")
+else:
+    print("Does not only digits")
+print()    
+#only alphapets
+n=input("Enter the name:")
+if n.isalpha():
+        print("only alphapets")
+else:
+    print("Does not only alphabets")
+print()
+#many times of "a"
+n=banana
+print(n.count("a"))
+print()
+#replace with"-"
+n="1234"
+print(n.replace("","_"))
+
+    
