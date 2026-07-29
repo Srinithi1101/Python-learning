8a3910cdfa15366298b2ede8b28be0fdb4a16e78
Srinithi1101 <srinithik47@gmail.com>
2026-07-29 19:28:29 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,47 @@
+# sum=0
+# i=1
+# while i<=n:
+#     sum=sum+i
+#     i=i+1
+#  print(sum)
+
+# n=int(input("Enter a number:"))
+# count=0
+# for i in range(1,n):
+#     if i%3==0:
+#         count+=1
+# print(count)   
+
+
+# employee_status=input("Enter the employee name:")
+# salary=int(input("Enter the salary: "))
+# if employee_status=="working":
+#     print("the employee is working")
+#     if salary >=25000:
+#         print("Senior employee")
+#     else:
+#         print("the employee is junior")
+# else:
+#     print("The employee is not working")        
+
+
+# n=int(input("enterthe value of n:"))
+# sum=0
+# for i in range(1,n+1):
+#     if i%3==0:
+#         sum=sum+i
+# print("sum of odd number=",sum)   
+
+# n=int(input("Enter the value of n:"))
+# sum=0
+# for i in range(1,n+1):
+#     if i%3==0:
+#         sum=sum+i
+# print("sum of even number=",sum)       
+
+
+# name=input("Enter the name:")
+# count=0
+# for letter in name:
+#     count=count+1
+# print("the number of letters :",count)    
