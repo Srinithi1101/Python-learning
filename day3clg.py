18b2c40d4335d068d2c84cff345b9c6ba0cc3b76
Srinithi1101 <srinithik47@gmail.com>
2026-07-29 19:25:38 +0530

    Add files via upload

---
+++ day3clg.py
@@ -0,0 +1,152 @@
+#Arithmetic opration
+a=int(input("Enter the frist number:"))
+b=int(input("Enter the second number:"))
+print("Sum",a+b)
+print("Difference",a-b)
+print("Product",a*b)
+print("Quotient",a/b)
+print()
+
+#Input & Output
+var1=input("Enter the Name:")
+var2=input("Enter the Department:")
+var3=input("Enter the Age:")
+print("Student Details")
+print("---------------")
+print("Name",var1)
+print("Department",var2)
+print("Age",var3)
+print()
+
+#Expressions
+length=float(input("Enter the length of the rectangle:"))
+width=float(input("Enter the width of the rectangle:"))
+area=length*width
+perimeter=2*(length*width)
+print("Area",area)
+print("perimeter",perimeter)
+print()
+
+#comparion and logical Oprators
+marks=int(input("Enter your Marks:"))
+result=marks>=35
+print("Passed:",result)
+print()
+
+#Assignment Operators
+c_val=100
+"""c_val=c_val+5
+print(c_val)"""
+c_val+=5
+print(c_val)
+c_val-=5
+print(c_val)
+c_val/=5
+print(c_val)
+
+
+# 1
+a=int(input("Enter the frist number:"))
+b=int(input("Enter the second number:"))
+print("Sum",a+b)
+print("Difference",a-b)
+print("Multiplication",a*b)
+print("Division",a/b)
+print()
+
+#2
+name=input("Enter the name:")
+age=int(input("Enter the age:"))
+print("Hello",name+".","You are",age,"years old")
+print()
+
+#3
+side=(input("Enter side:"))
+area=side*side
+print("Area of Square=",area)
+
+#4
+radius=(input("Enter radius"))
+C=2*3.14*radius
+print("Circumferance=",c)
+print()
+
+#5
+num1=int(input("Enter the first Number:"))
+num2=int(intput("Enter the second number:"))
+num3=int(input("Enter the first Number:"))
+Average=(Num1+num2+num3)/3
+print("Average=",Average)
+print()
+#6
+a=int(input("Enter the frist number:"))
+b=int(input("Enter the second number:"))
+print("The frist number is greater :",a>b)
+print("Both numbers are eqaul:",a=b)
+print()
+#7
+a=int(input("Enter the age:"))
+if age>=18:
+      print("Eligible to vote:")
+else:
+    print("Not eligible to vote:")
+print()
+#8
+P=input("Enter the Principal:")
+R=input("Enter the rate:")
+T=input("Enter the time:")
+si=(P*R*T)/100
+print("simple interest:",si)
+print()
+
+#9
+base=int(input("Enter the base:"))
+height=int(input("Enter the height:"))
+Area=(base*height)/2
+print("Area:",Area)
+print()
+#10
+a=input("Enter  first boolean(True/False):")=="True"
+b=input("Enter second boolean(True/False):")=="True"
+print("AND=",a and b)
+print("OR=",a or b)
+print("NOT of first=",not a)
+print("NOT of second=",not b)
+
+
+      
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
