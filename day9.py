5608263910778f49c64a0126f49d5540621e28be
Srinithi1101 <srinithik47@gmail.com>
2026-07-29 19:32:05 +0530

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,117 @@
+#String
+# name="sri nithi"
+# print(name.find("i"))
+# print(name.rfind("r"))
+# print(name.upper())
+# print(name.capitalize())
+# print(name.isalpha())
+# name="1234"
+# print(name.count("4"))
+# print(name.replace("2","/"))
+
+#string slicing
+# name="SRINITHI"
+# print(name[3:])
+# print(name[0:5:])
+# #2 step thalli
+# print(name[::2])
+# #reverse
+# print(name[::-1])
+# #starting
+# print(name[0::])
+
+# link="https//:google.com"
+# s=slice(8,-4)
+# print(link[s])
+
+#split()
+# name="S R I"
+# name="s,r,i"
+# name=name.split(",")
+# print(name)
+
+#joinn
+# name=["s","r","i"]
+# name="".join(name)
+# print(name)
+#task1
+# string=input("Enter:")
+# if string.isalpha():
+#     print("Alpapet")
+# else:
+#     print("is not alpha ")        
+
+#task 2
+# name=input("Enter the string:")
+# print(name.isalpha())
+
+#python collection
+
+#list
+names=["sri","nithi","k"]
+# print(names[0])
+# print(names[::-1])
+# names[0]="david"
+# print(names)
+# #append
+# names.append("Wick")
+# print(names)
+# #insert
+# names.insert(1,"Tony")
+# print(names)
+# #remove
+# names.remove("Tony")
+# print(names)
+#last elements delete
+# names.pop()
+# print(names)
+
+# last to frst(last name add panna frst varum)
+# names.sort()
+# print(names)
+
+# names.reverse()
+# print(names)
+
+# # string name
+# print(len(names))
+# print(names)
+
+# # membership operator
+# print("sri" in names)
+# print("kreethi" in names )
+
+# tuples
+# names=("Dave","gray","john","Dave")
+# print(names)
+# print(names(0))
+# names[0]="david"
+# print(names)
+# #count
+# # print(names.count("Dave"))
+# print(names.index("gray"))
+# print(len(names))
+
+#set
+# names={"dave","tony","john","john"}
+#print(names)
+# names.add("sri")
+# names.discard("bro")
+# names.remove("gray")
+# print(names)
+
+#task 1
+# list=["apple","banana","orange"]
+# n=input("Enter the value:")
+# if n in list:
+#     print("valid")
+# else:
+#     print("Not valid")
+
+# num=("1,2,4,5")
+# n=int(input("Enter the number:"))
+# if n in num:
+#     print("list")
+# else:
+#     print("Empty list")    
+ 
