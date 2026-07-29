08030e424c86b34e1450b6950ca66ccb9c5d9269
Srinithi1101 <srinithik47@gmail.com>
2026-07-29 19:26:01 +0530

    Add files via upload

---
+++ day3home.py
@@ -0,0 +1,75 @@
+# 1
+a=int(input("Enter the frist number:"))
+b=int(input("Enter the second number:"))
+print("Sum",a+b)
+print("Difference",a-b)
+print("Multiplication",a*b)
+print("Division",a/b)
+print()
+
+#2
+name=input("Enter the name:")
+age=int(input("Enter the age:"))
+print("Hello",name+".","You are",age,"years old")
+print()
+
+#3
+side=float(input("Enter side:"))
+area=side*side
+print("Area of Square=",area)
+print()
+
+#4
+radius=float(input("Enter radius:"))
+C=2*3.14*radius
+print("Circumferance=",C)
+print()
+
+#5
+num1=int(input("Enter the first Number:"))
+num2=int(input("Enter the second Number:"))
+num3=int(input("Enter the third Number:"))
+Average=(num1+num2+num3)/3
+print("Average=",Average)
+print()
+#6
+a=int(input("Enter the frist number:"))
+b=int(input("Enter the second number:"))
+print("The frist number is greater :",a>b)
+print("Both numbers are eqaul:",a==b)
+print()
+#7
+a=int(input("Enter the age:"))
+if age>=18:
+      print("Eligible to vote:")
+else:
+    print("Not eligible to vote:")
+print()
+#8
+P=float(input("Enter the Principal:"))
+R=float(input("Enter the rate:"))
+T=float(input("Enter the time:"))
+si=(P*R*T)/100
+print("simple interest:",si)
+print()
+
+#9
+base=int(input("Enter the base:"))
+height=int(input("Enter the height:"))
+Area=(base*height)/2
+print("Area:",Area)
+print()
+#10
+a=input("Enter  first boolean(True/False):")=="True"
+b=input("Enter second boolean(True/False):")=="True"
+print("AND=",a and b)
+print("OR=",a or b)
+print("NOT of first=",not a)
+print("NOT of second=",not b)
+
+
+      
+      
+
+
+
