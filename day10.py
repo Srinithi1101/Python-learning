28b8c3e3beb27ffa270433e304c257e23249b7a4
Srinithi1101 <srinithik47@gmail.com>
2026-07-29 19:33:51 +0530

    Add files via upload

---
+++ day10.py
@@ -0,0 +1,20 @@
+#dictionary
+capitals={"USA":"Washington DC","INDIA":"New Delhi","UK":"London"}
+# print(capitals.get("USA"))
+#update pannurathu
+# print(capitals.update({"Germany":"Berlin"}))
+# print(capitals)
+#last add 
+# print(capitals.update({"UK":"Las vegas"}))
+# print(capitals)
+# capitals.pop("UK")
+#capitals.popitem()
+#print(capitals)
+#full clear
+# capitals.clear()
+# print(capitals.keys())
+# print(capitals.values())
+# print(capitals.item())
+
+# for key ,value in capitals.items():
+    #  print(key,":",value)
